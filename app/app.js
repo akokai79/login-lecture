328ab089f82a522b41411696cb05d6bb7942844d
name <usermail@gmail.com>
2021-02-07 20:29:36 +0900

    login #2

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@
 
 // 모듈
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express( );
 
 const port = 3000;
@@ -12,10 +13,11 @@ const home = require("./src/routes/home");
 // 앱 세팅
 app.set("views","./src/views");
 app.set("view engine","ejs");
-
 app.use(express.static(`${__dirname}/src/public`)); // 
-app.use("/", home); // 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use("/", home); // 
 
 module.exports = app;
 
--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -10,6 +10,24 @@ function login() {
     const req = {
         id: id.value,
         pw: pw.value,
-    }
-    console.log(req);
+    };    
+
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req),
+    }).then((res) => res.json())
+      .then((res) => {
+          if (res.success) {
+              location.href = "/";
+          } else {
+              alert(res.msg);
+          }
+      } )
+      .catch((err) => {
+          console.error(new Error("로그인 중 에러 발생"));
+      });
+
 }
