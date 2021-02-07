af7931104536687877b3b74868a58f6da3325f27
name <usermail@gmail.com>
2021-02-07 18:07:20 +0900

    DOM #1

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log("hi");
+
+const id = document.querySelector("#id"),
+      pw = document.querySelector("#pw"),
+      login_btn = document.querySelector("button");
+
+      login_btn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        pw: pw.value,
+    }
+    console.log(req);
+}
